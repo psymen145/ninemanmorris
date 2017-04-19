ff1197de5135dbb66d035e51019649ae3a000157
Simon <sim.liu92@gmail.com>
2017-04-19 13:05:41 -0400

    Update board.h

--- board.h
+++ board.h
@@ -5,25 +5,23 @@
 
 using namespace std;
 
-class board{
-    
+class board {
+
 public:
-    
-    board();
-    void displayBoard();
-    
-    //set status of board
-    void setBoard(int location, int player);
-    //gets 0,1,2 (0 means empty, 1 means player 1, 2 means player 2)
-    int getBoard(int position);
-    bool adjacentPositions(int token, int position);
-    int getTokensPlacedP1();
-    int getTokensPlacedP2();
-    
+
+	board();
+	void displayBoard();
+
+	//set status of board
+	void setBoard(int location, int player);
+	//gets 0,1,2 (0 means empty, 1 means player 1, 2 means player 2)
+	int getBoard(int position);
+	bool adjacentPositions(int token, int position);
+
 private:
-    
-    int gameboard[24];
-    
+
+	int gameboard[24];
+
 };
 
 #endif
