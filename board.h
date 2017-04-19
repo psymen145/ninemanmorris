ff40dcd1b52133cc8d7a823b0c9a0b130840578e
Simon <sim.liu92@gmail.com>
2017-04-19 16:48:14 -0400

    Update board.h

--- board.h
+++ board.h
@@ -1,22 +1,22 @@
-#ifndef board_h
-#define board_h
+
+#ifndef BOARD_H
+#define BOARD_H
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-class board {
+class Board {
 
 public:
 
-	board();
+	Board(int arr[] = NULL);
 	void displayBoard();
-
-	//set status of board
-	void setBoard(int location, int player);
-	//gets 0,1,2 (0 means empty, 1 means player 1, 2 means player 2)
-	int getBoard(int position);
-	bool adjacentPositions(int token, int position);
+	void setBoard(int location, int player);	//set status of board
+	int getBoard(int position);		//returns 0,1,2 (0 means empty, 1 means player 1, 2 means player 2)
+	bool adjacentPositions(int token, int position);	//returns true if adjacent position
+	int evaluateBoard(int maximizingPlayer, int phase);		//returns a score, used by alpha beta
+	vector<Board> generateBoard(bool maximizingPlayer, int phase);	//generates a board for alphabeta
 
 private:
 
